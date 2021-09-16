732dd68a87bebba6f7e29924846f703d817f5928
Arkajit Choudhury <arkajitchoudhury54@gmail.com>
2021-09-17 03:50:50 +0530

    sideBar done

--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -3,11 +3,21 @@ import React from 'react';
 import "./Sidebar.css";
 
 function SideBar() {
+
+    const recentItem = function(topic) {
+        return(
+            <div className="sidebar_recentItem">
+                <span className="sidebar_hash">#</span>
+                <p>{topic}</p> 
+            </div>
+        );
+    };
+
     return (
-        <div className="Sidebar">
+        <div className="sidebar">
             <div className="sidebar_top">
-                <img src="" alt="" />
-                <Avatar />
+                <img src="https://timelinecovers.pro/facebook-cover/download/simple-background-that-can-be-texted-facebook-cover.jpg" alt="" />
+                <Avatar className="sidebar_avatar" />
                 <h2>Arkajit Choudhury</h2>
                 <h4>arlachoudhury@gmail.com</h4>
             </div>
@@ -20,7 +30,7 @@ function SideBar() {
                     </p>
                 </div>
                 <div className="sidebar_stat">
-                    <p>who viwed you</p>
+                    <p>Views on your post</p>
                     <p className="sidebar_statnumber">
                         2,569
                     </p>
@@ -28,7 +38,12 @@ function SideBar() {
             </div>
 
             <div className="sidebar_bottom">
-                
+                <p>Recent</p>
+                {recentItem("React")}
+                {recentItem("Angular")}
+                {recentItem("FAANG")}
+                {recentItem("AWS")}
+                {recentItem("Software Engineering")}
             </div>
         </div>
     )
